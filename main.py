3fb1db2998edf3160fbc4f04689570ac06abba75
Facitest <ricardorubio.aus@gmail.com>
2025-04-30 14:36:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from fastapi import FastAPI, Form
+import uuid
+import os
+import replicate
+from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+
+load_dotenv()
+
+app = FastAPI()  # <- ESTA LÍNEA DEBE ESTAR ANTES DE app.mount()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
